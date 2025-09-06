c34546738d9256ac4ec3b38e8fc6f43736a92d7e
Taha <taha@Tahas-MacBook-Air.local>
2025-09-06 15:04:37 +0530

    Feat: Basic Setup of User

--- user/app.js
+++ user/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+const userRoutes = require('./routes/user.routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+
+app.use('/', userRoutes)
 
 
 
---
+++ user/controllers/user.controller.js
@@ -0,0 +1,27 @@
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+module.exports.register = async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        const user = await userModel.findOne({email});
+
+        if(user){
+            return res.status(400).json({ message: 'User already exists'});
+        }
+
+        const hash = await bcrypt.hash(password, 10);
+        const newUser = new userModel({name, email, password});
+
+        await newUser.save();
+
+        const token = jwt.sign({id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '10h'});
+
+        res.cookie('token', token);
+
+        res.send({ message: 'User registered successfully'});
+    }catch(error){
+        return res.status(500).json({ message: error.message});
+    }
+}
